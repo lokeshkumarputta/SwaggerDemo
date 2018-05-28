9cfa30b3ea018ac971a4883767272dbea9f70a8b
Lokeshkumar <lokeshkumarputta@gmail.com>
2018-05-28 23:19:55 +0530

    Need to fix

--- src/main/java/com/swagger/SwaggerDemo/SwaggerConfiguration.java
+++ src/main/java/com/swagger/SwaggerDemo/SwaggerConfiguration.java
@@ -1,9 +1,11 @@
 package com.swagger.SwaggerDemo;
 
+import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.google.common.base.Predicate;
 
@@ -11,13 +13,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfiguration {
 
 	@Bean
